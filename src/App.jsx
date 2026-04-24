8167898b0874ed8c9b61a1767396ae18aa0741b5
saipawar22 <sp1913684@gmail.com>
2026-04-24 19:42:19 +0530

    add user list and add users url and use to add new users

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,24 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { NavLink, Route, Routes } from "react-router";
+import AddUser from "./AddUser";
+import Userlist from "./Userlist";
 
-function App() {
-  const [usersData, setUsersData] = useState([]);
-
-  useEffect(() => {
-    getUsersData();
-  }, []);
-
-  const getUsersData = async () => {
-    const url = "https://dummyjson.com/users";
-    let response = await fetch(url);
-    response = await response.json();
-    // Setting only the 'users' array from the response [00:08:38]
-    setUsersData(response.users);
-  };
-
-  return (
-    <div className="App">
-      <h1>Fetch Data from API</h1>
-      {/* Header for the list */}
-      <ul className="user-list user-list-head">
-        <li>First Name</li>
-        <li>Last Name</li>
-        <li>Age</li>
-      </ul>
-
-      {/* Mapping through the users data to display it [00:09:27] */}
-      {usersData.length > 0 && usersData.map((user) => (
-        <ul key={user.id} className="user-list">
-          <li>{user.firstName}</li>
-          <li>{user.lastName}</li>
-          <li>{user.age}</li>
-        </ul>
-      ))}
-    </div>
-  );
-}
-
+function App(){
+  return(
+   <div>
+    <ul className="nav-list">
+      <li>
+        <NavLink to="/">list</NavLink>
+      
+      </li>
+      <li>
+        <NavLink to="/add">add</NavLink>
+      </li>
+    </ul>
+  <Routes>
+    <Route path="/" element={<Userlist/>}/>
+        <Route path="/add" element={<AddUser/>}/>
+  </Routes>
+   </div>
+  )
+} 
 export default App;
